f360b2c68cf04723f977bf5fb2ac4ceaa02fb885
FantaCy <19349041+CongFang@users.noreply.github.com>
2021-04-29 08:59:25 +0800

    Update test_on_dataset3_baseline.py

--- test_on_dataset3_baseline.py
+++ test_on_dataset3_baseline.py
@@ -109,18 +109,13 @@ def test(model, test_dataloader, config, model_num):
 
 
 if __name__ == "__main__":
-    remove_items = ['医务人员接触感染', '心梗', '脑梗', '心脑血管病',
-    '慢性消化系统疾病','外周血管病', '慢性呼吸道疾病', 
-    '自身免疫或结缔组织病', '服用糖皮质激素强的松15mgd×30天以上', 
-    '发病第几天出现气喘','咯血','ThTs','淋巴细胞CD45CD45abs', 
-    '咳嗽','HCRP', 'PCT','肌钙蛋白','BNP'
-    ]
+    remove_items = []
 
     config = edict()
     # path setting
-    self_pretrained_model_path = '/home/cfang/works/COVID-19/PMP/code/checkpoints/pretrained_models_on_dataset1_5_fold'
+    self_pretrained_model_path = ''
     model1, model2, model3, model4, model5, config = load_checkpoint(self_pretrained_model_path)
-    config.data_root = '/home/cfang/works/COVID-19/PMP/data/'
+    config.data_root = ''
 
     check_dir = time.strftime('%Y-%m-%d %H:%M:%S')
     os.mkdir(os.path.join('res', 'test_res_on_dataset3_'+check_dir))
